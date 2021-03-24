fce072a4125c70c02442a2fd2c5435ab0b4598f0
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-24 03:06:33 +0000

    card de home

--- src/js/layout.js
+++ src/js/layout.js
@@ -25,10 +25,10 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
+						<Route exact path="/people">
 							<Demo />
 						</Route>
-						<Route exact path="/single/:theid">
+						<Route exact path="/planets">
 							<Single />
 						</Route>
 						<Route>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,45 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+
+    //onClick={() => actions.changeColor(index, "orange")}
+    return (
+
+        <div className="container mt-5">
+
+            <div class="card-columns m-5 animated fadeIn">
+
+
+                {store.people.map((item, index) => {
+                    return (
+
+                        <div class="card-columns m-5 animated fadeIn">
+                            <div key={index} class="card puntero">
+                                <img class="card-img-top" src="https://i.postimg.cc/qqbdCbvC/sw.jpg" alt="Card image cap"/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{item.name}</h5>
+                                        <p class="card-text">
+                                            {item}
+                                        </p>
+                                        <button className="btn btn-success" >Change Color</button>
+                                    </div>
+                            </div>
+                        </div>
+
+					);
+				})}
+                </div>
+
+
+                        <Link to="/">
+                            <button className="btn btn-primary">Back home</button>
+                        </Link>
+
+
+        </div>
+
+    );
+}
+
